279956bd10ea894f1972fa927f000d01644da417
damifur <damian.a.furman@gmail.com>
2017-05-17 15:09:08 -0300

    El script ahora trata de buscar .wavs que se llamen como los fonos y los concatena

--- tts.py
+++ tts.py
@@ -1,11 +1,32 @@
 import sys
+import wave
 
-input = sys.argv[1]
-ans = ["-" + input[0]]
+data= []
+for infile in infiles:
+        w = wave.open(infile, 'rb')
+        data.append( [w.getparams(), w.readframes(w.getnframes())] )
+        w.close()
 
-for i in range(len(input) - 1):
-    ans.append(input[i] + input[i + 1])
+output = wave.open(outfile, 'wb')
+output.setparams(data[0][0])
+output.writeframes(data[0][1])
+output.writeframes(data[1][1])
+output.close()
 
-ans.append(input[-1] + "-")
+inputWord = sys.argv[1]
+outputFile = sys.argv[2]
+ans = ["-" + inputWord[0]]
+
+for i in range(len(inputWord) - 1):
+    ans.append(inputWord[i] + inputWord[i + 1])
+
+ans.append(inputWord[-1] + "-")
+
+for phono in ans:
+    w = wave.open(phono, 'rb')
+    data.append( [w.getparams(), w.readframes(w.getnframes())] )
+    w.close()
+
+output = wave.open(outputFile)
 
 print ans
