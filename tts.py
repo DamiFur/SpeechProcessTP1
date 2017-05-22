295e37fd1a3d2f392ec67cb406faafeeb173c9f7
damifur <damian.a.furman@gmail.com>
2017-05-22 14:20:21 -0300

    dos nuevos textgrids

--- tts.py
+++ tts.py
@@ -15,7 +15,13 @@ for i in range(len(inputWord) - 1):
 ans.append(inputWord[-1] + "-")
 
 diphones_to_wav = {
-    "ka" : "wav/jmp/diphones/ka_kasAka.wav"
+    "-k" : "wav/jmp/diphones/_k_kasAka.wav", 
+    "ka" : "wav/jmp/diphones/ka_kasAka.wav",
+    "ak" : "wav/jmp/diphones/ak_kasAka.wav",
+    "a-" : "wav/jmp/diphones/a__kasAka.wav",
+    "as" : "wav/jmp/diphones/as_kasAka.wav",
+    "sA" : "wav/jmp/diphones/sA_kasAka.wav",
+    "Ak" : "wav/jmp/diphones/Ak_kasAka.wav"
 }
 
 resp = AudioSegment.silent()
@@ -23,7 +29,7 @@ resp = AudioSegment.silent()
 for phono in ans:
     #w = wave.open(phono, 'rb')
     #data.append( [w.getparams(), w.readframes(w.getnframes())] )
-    diphone = AudioSegment.from_wav(diphones_to_wav["ka"])
+    diphone = AudioSegment.from_wav(diphones_to_wav[phono])
     resp = resp + diphone
     
 play(resp)
