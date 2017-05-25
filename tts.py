89e47adc9d891348deda2c7f3bfcb5be5e3bae8b
Juan Manuel Perez <jmperez.85@gmail.com>
2017-05-25 02:05:35 -0300

    Meto configuración en archivo.

--- tts.py
+++ tts.py
@@ -1,12 +1,19 @@
+"""Archivo principal del proyecto."""
 import sys
-import wave
+import ConfigParser
+
 from pydub import AudioSegment
 from pydub.playback import play
 
-data= []
+config = ConfigParser.ConfigParser()
+config.optionxform = str
+config.read("config.ini")
+voice = "JMP"
+
 
 inputWord = sys.argv[1]
 outputFile = sys.argv[2]
+
 ans = ["-" + inputWord[0]]
 
 for i in range(len(inputWord) - 1):
@@ -14,45 +21,12 @@ for i in range(len(inputWord) - 1):
 
 ans.append(inputWord[-1] + "-")
 
-diphones_to_wav = {
-    "-k" : "wav/jmp/diphones/_k_kas_aka.wav", 
-    "ka" : "wav/jmp/diphones/ka_kas_aka.wav",
-    "ak" : "wav/jmp/diphones/ak_p_alaka.wav",
-    "a-" : "wav/jmp/diphones/a__kas_aka.wav",
-    "as" : "wav/jmp/diphones/as_kas_aka.wav",
-    "sA" : "wav/jmp/diphones/sA_kas_aka.wav",
-    "Ak" : "wav/jmp/diphones/Ak_kas_aka.wav",
-    "-s" : "wav/jmp/diphones/_s_sak_ala.wav",
-    "kA" : "wav/jmp/diphones/kA_sak_ala.wav",
-    "-m" : "wav/jmp/diphones/_m_map_ala.wav",
-    "mA" : "wav/jmp/diphones/mA_m_apala.wav",
-    "Ap" : "wav/jmp/diphones/Ap_m_apala.wav",
-    "pa" : "wav/jmp/diphones/pa_m_apala.wav",
-    "al" : "wav/jmp/diphones/al_m_apala.wav",
-    "la" : "wav/jmp/diphones/la_m_apala.wav",
-    "ma" : "wav/jmp/diphones/ma_map_ala.wav",
-    "ap" : "wav/jmp/diphones/ap_map_ala.wav",
-    "pA" : "wav/jmp/diphones/pA_map_ala.wav",
-    "Al" : "wav/jmp/diphones/Al_map_ala.wav",
-    "-p" : "wav/jmp/diphones/_p_p_alaka.wav",
-    "lA" : "wav/jmp/diphones/lA_pal_aka.wav",
-    "am" : "wav/jmp/diphones/am_pam_asa.wav",
-    "sa" : "wav/jmp/diphones/sa_pam_asa.wav",
-    "As" : "wav/jmp/diphones/As_pam_asa.wav",
-    "Am" : "wav/jmp/diphones/Am_p_amasa.wav",
-    "A-" : "wav/jmp/diphones/A__pamas_a.wav",
-    "-l" : "wav/jmp/diphones/_l_lam_asa.wav",
-
-}
 
 resp = AudioSegment.silent()
 
 for phono in ans:
-    #w = wave.open(phono, 'rb')
-    #data.append( [w.getparams(), w.readframes(w.getnframes())] )
-    diphone = AudioSegment.from_wav(diphones_to_wav[phono])
+    print(phono)
+    diphone = AudioSegment.from_wav(config.get(voice, phono))
     resp = resp + diphone
-    
-play(resp)
 
-print(ans)
+play(resp)
