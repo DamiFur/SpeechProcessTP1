d548b29847b47a95b4ca9393c5b1bc93b4996670
damifur <damian.a.furman@gmail.com>
2017-05-29 00:29:21 -0300

    questionify toma como entrada el nombre del .PitchTier a modificar y el nombre del archivo .PitchTire donde va a guardar el resultado

---
+++ scripts/manipular-pitch/questionify.py
@@ -0,0 +1,34 @@
+import sys
+
+file_name = sys.argv[1]
+output_file = sys.argv[2]
+
+#def questionify_pitch(file_name):
+with open(file_name, 'rb') as f:
+    points = 0
+    text = f.read()
+    lines = text.split('\n')
+    for line in lines:
+        if "points: size" in line:
+            points = int(line.split("= ",1)[1])
+            break
+
+    limit = points * 0.85
+
+    pointvalues = text.split("points [")
+
+    i = 0
+    ans = pointvalues[0]
+    for p in pointvalues:
+        if i > limit:
+            aux = p.split("value = ", 1)
+            tmp = float(aux[1])
+            tmp = tmp * 1.2
+            ans += "points [" + aux[0] + "value = " + str(tmp) + "\n"
+        else:
+            if i != 0:
+                ans += "points [" + p
+        i += 1
+    
+with open(output_file, 'w') as output:
+    output.write(ans)
