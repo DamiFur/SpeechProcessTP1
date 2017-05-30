db5c402ccc9c8bcc7ce655898f64cd00e58bc3cf
damifur <damian.a.furman@gmail.com>
2017-05-30 10:03:03 -0300

    mejorado el questionify.py

--- scripts/manipular-pitch/questionify.py
+++ scripts/manipular-pitch/questionify.py
@@ -13,18 +13,25 @@ with open(file_name, 'rb') as f:
             points = int(line.split("= ",1)[1])
             break
 
-    limit = points * 0.85
+    limit1 = points * 0.80
+    limit2 = points * 0.93
 
     pointvalues = text.split("points [")
 
     i = 0
     ans = pointvalues[0]
+    constant = 1.05
     for p in pointvalues:
-        if i > limit:
+        if i > limit1:
             aux = p.split("value = ", 1)
             tmp = float(aux[1])
-            tmp = tmp * 1.1
+            tmp = tmp * constant
+            if i < limit2:
+                constant += 0.02
+            else:
+                constant -= 0.04
             ans += "points [" + aux[0] + "value = " + str(tmp) + "\n"
+            print constant
         else:
             if i != 0:
                 ans += "points [" + p
