409923137859f7982c716bf22ed12dfdda00db55
Juan Manuel Perez <jmperez.85@gmail.com>
2017-06-06 09:21:10 -0300

    work in progress.

--- tts.py
+++ tts.py
@@ -5,6 +5,12 @@ import ConfigParser
 from pydub import AudioSegment
 from pydub.playback import play
 
+from utils.questionify import questionify
+
+config = ConfigParser.ConfigParser()
+config.optionxform = str
+config.read("config.ini")
+
 
 def split_word_into_diphones(word):
     """Get a list of diphones needed to synthesize the word."""
@@ -18,21 +24,36 @@ def split_word_into_diphones(word):
     return ans
 
 
-def synthesize(word, voice):
-    """Synthesize the word using diphones"""
-    config = ConfigParser.ConfigParser()
-    config.optionxform = str
-    config.read("config.ini")
-
+def _synthesize_non_question(word, voice):
     word_diphones = split_word_into_diphones(word)
 
-    resp = AudioSegment.silent()
+    resp = AudioSegment.silent(0)
 
     for phono in word_diphones:
         diphone = AudioSegment.from_wav(config.get(voice, phono))
         resp = resp + diphone
 
     return resp
+
+
+def synthesize(word, voice):
+    """Synthesize the word using diphones"""
+    is_question = False
+
+    if word[-1] == '?':
+        is_question = True
+        word = word[:-1]
+
+    resp = _synthesize_non_question(word, voice)
+
+    if is_question:
+        limit1 = resp.duration_seconds * 0.8
+        limit2 = resp.duration_seconds * 1.1
+
+        resp = questionify(resp, limit1, limit2, increase_factor=0.2)
+
+    return resp
+
 
 if __name__ == "__main__":
     word = sys.argv[1]
@@ -42,4 +63,3 @@ if __name__ == "__main__":
 
     play(resp)
     resp.export(outfile, format="wav")
-
---
+++ utils/questionify.py
@@ -0,0 +1,44 @@
+"""Questionify."""
+import tempfile
+import copy
+import numpy as np
+from pydub import AudioSegment
+from pitch_tier import get_pitch_tier, replace_pitch_tier
+
+
+def _questionify_tier(tier, limit1, limit2, increase_factor=0.2):
+    time, values = tier["time"], tier["values"]
+
+    """ Hice la cuenta de esto.."""
+    mu = values.mean()
+    a = (-increase_factor * mu) / float((limit2 - limit1) / 2)**2
+
+    def f(x):
+        if limit1 <= x and x <= limit2:
+            return a * (x - limit1) * (x - limit2)
+        else:
+            return 0
+
+    fv = np.vectorize(f)
+
+    x = fv(time)
+
+    new_tier = copy.deepcopy(tier)
+
+    new_tier['values'] = x + values
+
+    return new_tier
+
+
+def questionify(audio_segment, limit1, limit2, increase_factor=0.2):
+    """Questionify wav"""
+    tmp_wav = tempfile.NamedTemporaryFile(suffix=".wav", delete=False)
+    audio_segment.export(tmp_wav.name, format=".wav")
+
+    wav_path = tmp_wav.name
+
+    tier = get_pitch_tier(tmp_wav.name)
+    question_tier = _questionify_tier(tier, **kwargs)
+    replace_pitch_tier(wav_path, question_tier, wav_path)
+
+    return AudioSegment.from_file(wav_path)
